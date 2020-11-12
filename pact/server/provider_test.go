076cc7ef5d5a0cec0b32511f2e2ab11dc285c583
Kevin Denver <kevin.denver@gmail.com>
2020-11-12 09:31:15 +0000

    Add support for existing todo state

--- pact/server/provider_test.go
+++ pact/server/provider_test.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"github.com/pact-foundation/pact-go/dsl"
 	"github.com/pact-foundation/pact-go/types"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 )
@@ -18,10 +20,24 @@ var (
 	repository      TodoRepository
 	serverAddr      = fmt.Sprintf("localhost:%d", pactServerPortAddr)
 	providerBaseURL = fmt.Sprintf("http://localhost:%d", pactServerPortAddr)
-	pactUrls        = []string{
-		filepath.FromSlash("pacts/jvm_todo_client-todo_api.json"),
-	}
+	pactUrls        = findPactFiles("pacts")
 )
+
+func findPactFiles(root string) []string {
+	files, err := ioutil.ReadDir(root)
+	if err != nil {
+		panic(err)
+	}
+
+	var filtered = make([]string, 0)
+	for _, fileInfo := range files {
+		if strings.HasSuffix(fileInfo.Name(), ".json") {
+			filtered = append(filtered, filepath.Join(root, fileInfo.Name()))
+		}
+	}
+
+	return filtered
+}
 
 func startServer(repository TodoRepository, done chan bool) {
 	mux := NewTodoAPI(repository)
@@ -59,7 +75,7 @@ func TestMain(m *testing.M) {
 
 func TestProvider(t *testing.T) {
 	pact := &dsl.Pact{
-		Host: "localhost",
+		Host:     "localhost",
 		Provider: "TodoApi",
 	}
 
@@ -69,7 +85,19 @@ func TestProvider(t *testing.T) {
 		StateHandlers: types.StateHandlers{
 			// Setup any state required by the test
 			// in this case, we ensure there is a "user" in the system
-			"An empty repository": func() error {
+			"an empty repository": func() error {
+				return nil
+			},
+
+			"an existing todo with id=--MLqrG6LkLkkKc1iMLBt": func() error {
+				repository.Persist(Todo{
+					Id:       "-MLqrG6LkLkkKc1iMLBt",
+					Revision: "-MLivp1BrS59mMbSN7Jr",
+					Text:     "Don't forget the milk",
+					Status:   "TODO",
+					Category: "shopping",
+					Tags:     []string{"groceries", "food"},
+				})
 				return nil
 			},
 		},
